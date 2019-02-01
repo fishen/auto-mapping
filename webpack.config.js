3e293043ce8996c2a186ec3181db60725d769f79
yuhao <yuhao@zhiketong.cn>
2019-02-01 20:19:38 +0800

    change bundle way

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'production',
+    entry: './src/index.ts',
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.ts']
+    },
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname),
+        libraryTarget:'commonjs'
+    }
+};
